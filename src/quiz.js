9f5605a8dd7b2b9f8744b4547c3011bee6df9f47
PRANAV S V <pranavsv2004@gmail.com>
2024-02-10 23:11:38 +0530

    client finished, not divided

--- src/quiz.js
+++ src/quiz.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- <App />, root
+import React from "react";
+import ReactDOM from "react-dom";
+import Quiz from "./components/Quiz";
+
+const quizId = document.getElementById("quizRoot");
+
+ReactDOM.render(
+  <Quiz />, quizId  
 );
 
